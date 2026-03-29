f34917d605de577b6fd6fc455508efedcc42d16f
unknown <toptopgoinfo@gmail.com>
2026-03-30 00:37:17 +0100

    fix: servir fichiers statiques depuis la racine1

--- server.js
+++ server.js
@@ -4,12 +4,14 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(__dirname));
+// servir fichiers statiques
+app.use(express.static(path.join(__dirname, "public")));
 
+// route principale
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.listen(PORT, () => {
-  console.log("Server running on port " + PORT);
+  console.log(`Server running on port ${PORT}`);
 });
